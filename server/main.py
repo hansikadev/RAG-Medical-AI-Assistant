ab87d6aa0861221072b340673d1ef5a187f394ac
hansikadev <tanwarhansika123@gmail.com>
2026-06-14 23:17:17 +0530

    Add .env to .gitignore and implement vector store loading functionality

--- server/main.py
+++ server/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from middlewares.exception_handlers import catch_exception_middleware
-
+from routes.upload_pdfs import router as upload_router
+from routes.ask_question import router as ask_router
 
 
 
@@ -23,3 +24,7 @@ app.middleware("http")(catch_exception_middleware)
 
 # routers
 
+# 1. upload pdfs documents
+app.include_router(upload_router)
+# 2. asking query
+app.include_router(ask_router)
