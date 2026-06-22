cf417c48bd993a04302880ff672922fb625045aa
hansikadev <tanwarhansika123@gmail.com>
2026-06-22 15:38:00 +0530

    port fix

--- client/config.py
+++ client/config.py
@@ -1,3 +1,3 @@
-# API_URL="https://rag-medical-ai-assistant.onrender.com"
+API_URL="https://rag-medical-ai-assistant.onrender.com"
 
-API_URL="http://127.0.0.1:8000"
+# API_URL="http://127.0.0.1:8000"
