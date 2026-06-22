6b360206c2de9bb12e99e82382c293d95d4b4340
hansikadev <tanwarhansika123@gmail.com>
2026-06-22 12:21:51 +0530

    fix

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import uvicorn
+import os
+
+
 def main():
     print("Hello from medical-assisstant!")
 
 
 if __name__ == "__main__":
     main()
+
+
+if _name_ == "_main_":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        reload=False
+    )
