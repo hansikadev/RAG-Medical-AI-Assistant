dd035175f17b3c8bbfcf558b1114ddbbbc9ad372
hansikadev <tanwarhansika123@gmail.com>
2026-06-22 15:53:41 +0530

    build fix

--- server/modules/llm.py
+++ server/modules/llm.py
@@ -1,6 +1,3 @@
-from langchain_core.prompts import PromptTemplate
-from langchain_classic.chains import RetrievalQA
-from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
 
@@ -9,6 +6,10 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 def get_llm_chain(retriever):
+    from langchain_core.prompts import PromptTemplate
+    from langchain_classic.chains import RetrievalQA
+    from langchain_groq import ChatGroq
+
     llm = ChatGroq(
         groq_api_key=GROQ_API_KEY,
         model_name="llama-3.3-70b-versatile"
@@ -17,20 +18,20 @@ def get_llm_chain(retriever):
     prompt = PromptTemplate(
         input_variables=["context", "question"],
         template="""
-        You are **MediBot**, an AI-powered assistant trained to help users understand medical documents and health-related questions.
+        You are *MediBot*, an AI-powered assistant trained to help users understand medical documents and health-related questions.
 
-        Your job is to provide clear, accurate, and helpful responses based **only on the provided context**.
+        Your job is to provide clear, accurate, and helpful responses based *only on the provided context*.
 
         ---
 
-        🔍 **Context**:
+        🔍 *Context*:
         {context}0
    
-        🙋‍♂️ **User Question**:
+        🙋‍♂️ *User Question*:
         {question}
 
         ---
-        💬 **Answer**:
+        💬 *Answer*:
         - Respond in a calm, factual, and respectful tone.
         - Use simple explanations when needed.
         - If the context does not contain the answer, say: "I'm sorry, but I couldn't find relevant information in the provided documents."
